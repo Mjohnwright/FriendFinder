d5829a821230f98e1b7cce21967164b60e4ff05a
Mjohnwright <34377327+Mjohnwright@users.noreply.github.com>
2018-05-03 23:40:01 -0400

    still need to get modal to work

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -1,8 +1,6 @@
 // ===============================================================================
 // DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
+// Each object contains: name, photo link and survey score set in an array
 // ===============================================================================
 
 var friendsArray = [
@@ -17,63 +15,56 @@ var friendsArray = [
     photo:
       "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
     surveyScores: ["5", "2", "3", "2", "3", "1", "2", "4", "5", "4"]
-  }
-];
-
-var newUserArray = [
+  },
+  {
+    name: "Theresa",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
+    surveyScores: ["3", "1", "1", "4", "2", "5", "3", "2", "4", "4"]
+  },
+  {
+    name: "David",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
+    surveyScores: ["3", "2", "2", "1", "5", "3", "2", "2", "3", "5"]
+  },
+  {
+    name: "Joseph",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
+    surveyScores: ["5", "5", "2", "1", "2", "3", "1", "1", "3", "2"]
+  },
   {
     name: "Theodore",
     photo:
       "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
-    surveyScores: ["3", "1", "4", "4", "2", "2", "5", "4", "2", "3"]
-  }];
-
-
-function evaluate(array, array2) {
-  console.log("this is running");
-
-var bestMatch = {
-  name: 'name',
-  photo: 'www.testphoto.com',
-  pointDifference: 10000000000000
-};
-
-// / Iterate through each instance of User Responses
-  for (var i = 0; i < friendsArray.length; i++) {
-    // console.log('friendsArray = ' + JSON.stringify(friendsArray[i]));  // each object is dispayed in entirerty as string.
-    
-    var comparisonArray = []; //this will be used to evaluate the lowest matched score against the new user score
-    for (var y = 0; y < 10; y++) {
-      // console.log(" !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!45 the value of y = " + y);
-      // console.log('line 45 scores = ' + JSON.stringify(friendsArray[i].surveyScores[y]));  // each object is dispLayed in entirerty as string.
-      // console.log('line 46 scores = ' + JSON.stringify(newUserArray[0].surveyScores[y]));  // each object is dispLayed in entirerty as string.
-      var points = 0;
-      var dif = parseInt(friendsArray[i].surveyScores[y]) - parseInt(newUserArray[0].surveyScores[y]);//removes the string from integer
-      dif = Math.abs(dif);
-      points = dif + dif; //this eliminates negative numbers..the 'Math.absolute' method
-      comparisonArray.push(dif); // pushes the difference of this one instance to an array
-      console.log("line 53 comparison array = " + comparisonArray);
-        if (dif <= bestMatch.pointDifference) {
-          bestMatch.name = friendsArray[i].name;
-          console.log("line 58!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!best match name = " + bestMatch.name);
-        }
-        }
+    surveyScores: ["1", "4", "4", "4", "4", "2", "3", "1", "1", "3"]
+  },
+  {
+    name: "Jack",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
+    surveyScores: ["1", "1", "5", "3", "2", "3", "2", "3", "3", "3"]
+  },
+  {
+    name: "Sally",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
+    surveyScores: ["5", "3", "1", "3", "5", "5", "2", "3", "2", "4"]
+  },
+  {
+    name: "Yvette",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
+    surveyScores: ["2", "4", "4", "3", "3", "3", "4", "1", "3", "4"]
+  },
+  {
+    name: "Penelope",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.pexels.com%2Fusers%2Favatars%2F283045%2Fkaryme-carvalho-310.jpeg%3Fw%3D200%26h%3D200%26fit%3Dcrop%26crop%3Dfaces&imgrefurl=https%3A%2F%2Fwww.pexels.com%2Fphoto%2Fclose-up-photography-of-a-woman-s-face-839348%2F&docid=7LJIMIxwueaQFM&tbnid=3HyBXVdVwyHIVM%3A&vet=10ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA..i&w=200&h=200&bih=675&biw=1396&q=stock%20face%20photos%20200x200&ved=0ahUKEwjLhY_xy9_aAhVQTt8KHYd4CZcQMwg-KAgwCA&iact=mrc&uact=8",
+    surveyScores: ["5", "4", "3", "2", "2", "1", "1", "1", "3", "3"]
   }
-  
+];
 
-
-
-
-
-
-}
-
-//EXISTING SCORES
-
-
-evaluate(friendsArray, newUserArray);
-
-// var difference = Math.abs(user1 - user2);
-
-// Note how we export the array. This makes it accessible to other files using require.
+//Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,10 @@
 // ===============================================================================
 // DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
+//
 // ===============================================================================
-var path = require("path");
+// var path = require("path");
 
 var friendsData = require("../data/friendsData");
-
 
 // ===============================================================================
 // ROUTING
@@ -16,50 +15,46 @@ module.exports = function(app) {
   //FRIENDS API PAGE
   //Set this up for testing...it is working
   //This path will show all the friend user data in JSON format
-  app.get("/api/friendsArray", function(req, res) {
+  app.get("/api/friends", function(req, res) {
     res.json(friendsData);
   });
 
   // ---------------------------------------------------------------------------
-  //HANDLES NEW USER DATA (COMPARES NEW DAT TO EXISTING DATA AND PUSHES NEW DATA TO FRIENDSDATA )
-
-  app.post("/api/friendsArray", function(req, res) {
+  //HANDLES NEW USER DATA (COMPARES NEW DATA TO EXISTING DATA AND PUSHES NEW DATA TO FRIENDSDATA )
+  app.post("/api/friends", function(req, res) {
 
     // Assign the user survey to a variable
-    var newSurveyInput = req.body;
-    var UserScores = req.body.surveyScores;
-    console.log('userInput = ' + JSON.stringify(newSurveyInput));
-    console.log('userSCores= ' + UserScores);
+    var newSurveyInput = req.body; ///we can get this because of bodyParser middleware
+    var UserScores = newSurveyInput.surveyScores;
+    console.log("userInput = " + JSON.stringify(newSurveyInput));
+    console.log("userScores= " + UserScores);
 
-   
-    var bestMatch = {
-      name: 'name',
-      photo: 'www.testphoto.com',
-      pointDifference: 10000000000000
-    };
+    // Compute best friend match
+    var bestMatchName = "";
+    var bestMatchPhoto = "";
+    var totalDifference = 100000000000000000; 
 
-    
-    // / Iterate through each instance of User Responses
-  for (var i = 0; i < friendsData.length; i++) {
-    // console.log('friendsArray = ' + JSON.stringify(friendsArray[i]));  // each object is dispayed in entirerty as string.
-    
-    var comparisonArray = []; //this will be used to evaluate the lowest matched score against the new user score
-    for (var y = 0; y < UserScores.length; y++) {
-      var points = 0;
-      var dif = parseInt(UserScores[y]) - parseInt(friendsData[i].surveyScores[y]);//removes the string from integer
-      dif = Math.abs(dif);
-      points = dif + dif; //this eliminates negative numbers..the 'Math.absolute' method
-      // comparisonArray.push(dif); // pushes the difference of this one instance to an array
-      // console.log("line 53 comparison array = " + comparisonArray);
-        if (dif <= bestMatch.pointDifference) {
-          bestMatch.name = friendsData[i].name;
-          console.log("line 58!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!best match name = " + bestMatch.name);
-        }
-        }
-  }
+    // Iterate through each instance of User Responses
+    for (var i = 0; i < friendsData.length; i++) {
+      var dif = 0;
+      for (var y = 0; y < UserScores.length; y++) {
+        dif += Math.abs(friendsData[i].surveyScores[y] - UserScores[y]);
+      }
+      console.log("dif = " + dif);
 
+      //
+      if (dif < totalDifference) {
+        console.log("Closest match found = " + dif);
+        console.log("Friends name = " + friendsData[i].name);
+        console.log("Friend Photo = " + friendsData[i].photo);
+
+        totalDifference = dif;
+        bestMatchName = friendsData[i].name;
+        bestMatchPhoto = friendsData[i].photo;
+      }
+    }
 
     //pushes the data into friendsData.js ('friendsArray')
-    friendsData.push(newSurveyInput);});
-}
-
+    friendsData.push(newSurveyInput);
+  });
+};
